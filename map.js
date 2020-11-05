b9ad90b315d11ae9d362b15ba7f63e15bf6d26fc
Laaurent <48626779+Laaurent@users.noreply.github.com>
2020-11-05 17:01:37 +0100

    trees

--- map.js
+++ map.js
@@ -1,38 +1,83 @@
-var map = L.map("mapid", {
-    preferCanvas: true,
-  }).locate({setView: true, maxZoom: 70});
-  
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+if ("geolocation" in navigator) {
+  // La géolocalisation est disponible
+  console.log('OK geolocation');
 
-  if ( 'geolocation' in navigator) {
-    console.log('Geolocation disponible')
-    navigator.geolocation.getCurrentPosition(position => {
-        
-        console.log(position.coords.latitude) 
-        console.log(position.coords.longitude)
+  navigator.geolocation.getCurrentPosition( function( position ) {
+      // Connaître la position du navigateur
+      console.log(position.coords);
 
-        var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
-        marker.bindPopup("Vous etes ici.").openPopup();
+      // Afficher la carte
+      var myMap = L.map('myMap').setView([position.coords.latitude, position.coords.longitude], 20);
+      L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png').addTo(myMap);
 
-        var geojsonMarkerOptions = {
-            radius: 4,
-            fillColor: "#ff7800",
-            color: "#000",
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.8,
-          };
-          var arbres_lyr = L.geoJSON(arbres, {
+      // Ajouter un marker
+      var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(myMap);
 
-            pointToLayer: function (feature, latlng) {
-            
-            return L.circleMarker(latlng, geojsonMarkerOptions);
-            
-            }}).addTo(map);
-    });
-} else {
-    console.log('Geolocation non disponible')
+      getTrees(position.coords.latitude, position.coords.longitude)
+      .then( function(data){
+        console.log(data.records);
+
+        data.records.map( function (item){
+          var myIcon = L.divIcon({className: 'my-div-icon'});
+          L.marker([item.geometry.coordinates[1],item.geometry.coordinates[0]], {icon: myIcon}).addTo(myMap)
+        })
+      })
+      .catch( function(error){
+        console.error(error);
+      })
+  });
+} 
+else {
+  // La géolocalisation n'est pas disponible
+  console.log('NOT geolocation');
+};
+
+
+///
+function getgeoNavigation(){
+  return "geolocation" in navigator;
+}
+
+function getUserPosition(){
+  return new Promise( function(resolve, reject){
+      navigator.geolocation.getCurrentPosition( function( position ) {
+          // Connaître la position du navigateur
+          return resolve(position.coords);
+      });
+  })
+}
+
+function displayMap(lat, lng){
+  // Afficher la carte
+  var myMap = L.map('myMap').setView([lat, lng], 20);
+  L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png').addTo(myMap);
+
+  // Ajouter un marker
+  var marker = L.marker([lat, lng]).addTo(myMap);
 }
 
 
+///
 
+function getTrees(lat, lng) { 
+  return new Promise( function(resolve, reject){
+      fetch('https://opendata.paris.fr/api/records/1.0/search/?dataset=les-arbres&q=&rows=5000&facet=typeemplacement&facet=domanialite&facet=arrondissement&facet=libellefrancais&facet=genre&facet=espece&facet=varieteoucultivar&facet=circonferenceencm&facet=hauteurenm&facet=stadedeveloppement&facet=remarquable&geofilter.distance=48.8838%2C+2.2724%2C+1000')
+      .then( function( rawData ){
+          // Tester la requête
+          if( rawData.ok === true ){
+              return rawData.json()
+          }
+          else{
+              return reject(rawData)
+          }
+      })
+      .then( function( jsonData ){
+          return resolve( jsonData )
+      })
+      .catch( function( fetchError ){
+          return reject( fetchError )
+      });
+
+  })
+
+}
