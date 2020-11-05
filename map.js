94f52624ef2b947de526dbf1b0fd1c00acfa890a
Laaurent <48626779+Laaurent@users.noreply.github.com>
2020-11-05 14:24:24 +0100

    Map

---
+++ map.js
@@ -0,0 +1,21 @@
+var map = L.map("mapid", {
+    preferCanvas: true,
+  }).setView([48.866667, 2.333333], 13);
+  
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  if ( 'geolocation' in navigator) {
+    console.log('Geolocation disponible')
+    navigator.geolocation.getCurrentPosition(position => {
+        console.log(position.coords.latitude) 
+    console.log(position.coords.longitude)
+    });
+} else {
+    console.log('Geolocation non disponible')
+}
+
+var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(mymap);
+
